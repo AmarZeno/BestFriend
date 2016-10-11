3b1d52bc53336f16454888824035f5b66cdba669
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-11 13:42:36 -0600

    Added ball and trigger

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+
+    public GameObject infoboxCanvas;
+
+    void Awake() {
+        infoboxCanvas.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger Enter");
+        infoboxCanvas.SetActive(true);
+    }
+
+    void OnTriggerStay(Collider other) {
+        Debug.Log("Trigger stay");
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Trigger exit");
+        infoboxCanvas.SetActive(false);
+    }
+
+}
