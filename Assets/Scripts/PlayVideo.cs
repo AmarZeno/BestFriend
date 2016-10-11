44d4ead4ae285efdf84506654e1b05b58659741d
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-11 12:23:08 -0600

    New initial commit

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture background;
+
+	void Start () {
+
+        GetComponent<RawImage>().texture = background as MovieTexture;
+        background.Play();
+        background.loop = true; 
+	}
+	
+
+	void Update () {
+
+
+	
+	}
+}
