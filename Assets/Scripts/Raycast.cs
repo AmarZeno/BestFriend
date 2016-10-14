ba236b1527415374de8089f09929780fd1a6ecee
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-14 00:10:30 -0600

    Added cross hair

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -22,7 +22,7 @@ public class Raycast : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, 100))
         {
-            Debug.DrawRay(camera.position, camera.forward * 10, Color.green);
+           // Debug.DrawRay(camera.position, camera.forward * 10, Color.green);
 
             Debug.Log(hit.collider.name);
             if (hit.collider.name == "TV")
