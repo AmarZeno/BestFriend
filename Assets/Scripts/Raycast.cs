d054e05f4f233b7e973d6818fc747f7484c3bc8b
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-10-14 16:46:12 -0600

    Added lamp toggle

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -5,7 +5,11 @@ public class Raycast : MonoBehaviour {
 
     public GameObject infoboxCanvas;
     public GameObject tvTextPanel;
+    public GameObject lampLight;
+    public bool isLampLightCoroutineExecuting = false;
     public GameObject crossHair;
+
+    public float thickness = 0.5f;
 
     void Awake()
     {
@@ -22,21 +26,29 @@ public class Raycast : MonoBehaviour {
         Ray ray = new Ray(camera.position, camera.forward);
         RaycastHit hit;
 
+        // Using spherecast to increase the hit area
+        //  if (Physics.SphereCast(ray.origin, thickness, ray.direction, out hit))
+
         if (Physics.Raycast(ray, out hit, 100))
         {
-           // Debug.DrawRay(camera.position, camera.forward * 10, Color.green);
-
+            // Debug.DrawRay(camera.position, camera.forward * 10, Color.green);
             Debug.Log(hit.collider.name);
             if (hit.collider.name == "TV")
             {
                 infoboxCanvas.SetActive(true);
-                if (Input.GetMouseButton(0)) {
+                if (Input.GetMouseButton(0))
+                {
                     tvTextPanel.SetActive(true);
                     crossHair.SetActive(false);
                 }
             }
-            else
-            {
+            else if (hit.collider.name == "Lamp") {
+                infoboxCanvas.SetActive(true);
+                if (Input.GetMouseButton(0))
+                {
+                    StartCoroutine(ToggleLampLight());
+                }
+            } else  {
                 infoboxCanvas.SetActive(false);
 
                 // Hide all the active components if any
@@ -45,6 +57,23 @@ public class Raycast : MonoBehaviour {
                 // Show the crosshair
                 crossHair.SetActive(true);
             }
+
+
+        }
+    }
+
+    public IEnumerator ToggleLampLight()
+    {
+        if (isLampLightCoroutineExecuting == true)
+        {
+            yield return new WaitForSeconds(0.3f);
+        }
+        else
+        {
+            isLampLightCoroutineExecuting = true;
+            yield return new WaitForSeconds(0.3f);
+            lampLight.SetActive(!lampLight.activeSelf);
+            isLampLightCoroutineExecuting = false;
         }
     }
 }
