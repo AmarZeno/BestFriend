3d88c64ca6a5fd035b4dfc27ea2164ef15a98bef
Zeno <amarzeno@outlook.com>
2016-12-03 22:34:57 -0700

    Modified interactive text to phone convo

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -18,6 +18,7 @@ public class DialogueScript : MonoBehaviour {
     public GameObject ResponseText3;
     public GameObject ActionText;
     public GameObject SunBlocker;
+    public GameObject ConversationContainer;
 
     public Text displayText1;
     public Text displayText2;
@@ -30,6 +31,14 @@ public class DialogueScript : MonoBehaviour {
     public TextAsset sceneScript3;
     public TextAsset sceneScript4;
     public TextAsset sceneScript5;
+
+    public Canvas DigitalItemCanvas;
+    public Image ActionImage;
+    public Image ResponseImage1;
+    public Image ResponseImage2;
+    public Image ResponseImage3;
+
+    public Sprite[] ConversationSprites; 
 
     public string[] displayLines1;
     public string[] displayLines2;
@@ -47,12 +56,14 @@ public class DialogueScript : MonoBehaviour {
     int endLine =0;
     int currentLine = 0;
     int responseLine = 0;
+    int conversationSet = 0;
     int displayState = 5;
     int actionTextLine = 0;
 
     public bool textEnd = false;
     private bool canShowActionText = false;
     public bool canSurpassTV = false;
+    private bool isShowingConversation = false;
 
     // Padding constants
     const int responseTextPadding = 20;
@@ -72,6 +83,7 @@ public class DialogueScript : MonoBehaviour {
         ResponseText2.GetComponent<Graphic>().CrossFadeAlpha(0.01f, 0.01f, true);
         ResponseText3.GetComponent<Graphic>().CrossFadeAlpha(0.01f, 0.01f, true);
         ActionText.GetComponent<Graphic>().CrossFadeAlpha(0.01f, 0.01f, true);
+       
 
         if (sceneScript1 != null || sceneScript2 != null || sceneScript3 != null || sceneScript4 != null)
         {
@@ -88,13 +100,21 @@ public class DialogueScript : MonoBehaviour {
             soundTime = Time.timeSinceLevelLoad;
         }
 
-        updateTextLayout();
+       ConversationSprites = Resources.LoadAll<Sprite>("Sprites/");
+    }
+
+    void FixedUpdate() {
+        // Set Native Sizes
+        ActionImage.SetNativeSize();
+        ResponseImage1.SetNativeSize();
+        ResponseImage2.SetNativeSize();
+        ResponseImage3.SetNativeSize();
+
+        ShowAndCloseConversation();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-
 
         displayText1.text = displayLines1[responseLine];
         displayText2.text = displayLines2[responseLine];
@@ -102,7 +122,22 @@ public class DialogueScript : MonoBehaviour {
         displayText4.text = displayLines4[responseLine];
         displayText5.text = displayLines5[actionTextLine];
 
-        if(Time.timeSinceLevelLoad > soundTime + 8)
+        ActionImage.sprite = ConversationSprites[responseLine];
+
+        // Prevent array out of bounds
+        if (conversationSet <= 6)
+        {
+            ResponseImage1.sprite = ConversationSprites[8 + 0 + conversationSet];
+            ResponseImage2.sprite = ConversationSprites[8 + 1 + conversationSet];
+            ResponseImage3.sprite = ConversationSprites[8 + 2 + conversationSet];
+        }
+        else {
+            ResponseImage1.gameObject.SetActive(false);
+            ResponseImage2.gameObject.SetActive(false);
+            ResponseImage3.gameObject.SetActive(false);
+        }
+
+        if (Time.timeSinceLevelLoad > soundTime + 8)
         {
             ringtone.Play();
             soundTime = Time.timeSinceLevelLoad;
@@ -134,6 +169,7 @@ public class DialogueScript : MonoBehaviour {
             ResponseText2.GetComponent<Graphic>().CrossFadeAlpha(1.0f, fadeDuration, true);
             ResponseText3.GetComponent<Graphic>().CrossFadeAlpha(1.0f, fadeDuration, true);
             displayState++;
+            isShowingConversation = true;
         }
 
 
@@ -148,6 +184,7 @@ public class DialogueScript : MonoBehaviour {
             ActionText.GetComponent<Graphic>().CrossFadeAlpha(0.00f, 0.01f, true);
             actionTextLine = 2;
             displayState++;
+            isShowingConversation = false;
         }
 
         if (Time.timeSinceLevelLoad > currTime + 108.0f && canShowActionText == true)
@@ -160,6 +197,7 @@ public class DialogueScript : MonoBehaviour {
         if (Time.timeSinceLevelLoad > currTime + 118.0f && canShowActionText == true)
         {
             ActionText.GetComponent<Graphic>().CrossFadeAlpha(0.01f, 0.01f, true);
+            isShowingConversation = false;
         }
 
 
@@ -168,6 +206,7 @@ public class DialogueScript : MonoBehaviour {
         {
             textEnd = false;
             DenialSceneScript.textOver = true;
+            isShowingConversation = false;
         }
 
 
@@ -201,11 +240,9 @@ public class DialogueScript : MonoBehaviour {
         ResponseText1.GetComponent<Graphic>().CrossFadeAlpha(0.01f, fadeDuration, true);
         ResponseText2.GetComponent<Graphic>().CrossFadeAlpha(0.01f, fadeDuration, true);
         ResponseText3.GetComponent<Graphic>().CrossFadeAlpha(0.01f, fadeDuration, true);
-
+        isShowingConversation = false;
         currTime = Time.timeSinceLevelLoad;
         displayState = 0;
-
-        updateTextLayout();
 
     }
 
@@ -216,25 +253,52 @@ public class DialogueScript : MonoBehaviour {
             if (input == 1) { responseLine = 1; }
             if (input == 2) { responseLine = 2; }
             if (input == 3) { responseLine = 2; }
+            conversationSet = 3;
         }
         else if (responseLine == 1 || responseLine == 2)
         {
             if (input == 1) { responseLine = 3; }
             if (input == 2) { responseLine = 4; }
             if (input == 3) { responseLine = 4; }
+            conversationSet = 6;
         }
         else if (responseLine == 3 || responseLine == 4)
         {
             if (input == 1) { responseLine = 5; actionTextLine = 0; }
             if (input == 2) { responseLine = 6; actionTextLine = 1; }
             if (input == 3) { responseLine = 7; actionTextLine = 0; }
+            conversationSet = 9;
             canShowActionText = true;
             textEnd = true;
         }
     }
 
     void updateTextLayout() {
-        ResponseText2.transform.position = new Vector2(ResponseText1.transform.position.x + ResponseText1.GetComponent<RectTransform>().rect.width + responseTextPadding, ResponseText2.transform.position.y);
-        ResponseText3.transform.position = new Vector2(ResponseText2.transform.position.x + ResponseText2.GetComponent<RectTransform>().rect.width + responseTextPadding, ResponseText3.transform.position.y);
+        //   ResponseText2.transform.position = new Vector2(ResponseText1.transform.position.x + ResponseText1.GetComponent<RectTransform>().rect.width + responseTextPadding, ResponseText2.transform.position.y);
+        //  ResponseText3.transform.position = new Vector2(ResponseText2.transform.position.x + ResponseText2.GetComponent<RectTransform>().rect.width + responseTextPadding, ResponseText3.transform.position.y);
+        ActionImage.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width / 2) - (ResponseImage1.transform.GetComponent<RectTransform>().rect.width / 2) - 70, ActionImage.transform.localPosition.y);
+        ResponseImage1.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width/2) - (ResponseImage1.transform.GetComponent<RectTransform>().rect.width/2), ActionImage.transform.localPosition.y - ActionImage.transform.GetComponent<RectTransform>().rect.height - 10);
+        if (conversationSet == 3)
+        {
+            // Hack for wrong alignment in the second set
+            ResponseImage2.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width / 2) - (ResponseImage2.transform.GetComponent<RectTransform>().rect.width / 2), ResponseImage1.transform.localPosition.y - ResponseImage1.transform.GetComponent<RectTransform>().rect.height);
+            ResponseImage3.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width / 2) - (ResponseImage3.transform.GetComponent<RectTransform>().rect.width / 2), ResponseImage2.transform.localPosition.y - ResponseImage2.transform.GetComponent<RectTransform>().rect.height - 20);
+        }
+        else {
+            ResponseImage2.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width / 2) - (ResponseImage2.transform.GetComponent<RectTransform>().rect.width / 2), ResponseImage1.transform.localPosition.y - ResponseImage1.transform.GetComponent<RectTransform>().rect.height - 10);
+            ResponseImage3.transform.localPosition = new Vector2((DigitalItemCanvas.GetComponent<RectTransform>().rect.width / 2) - (ResponseImage3.transform.GetComponent<RectTransform>().rect.width / 2), ResponseImage2.transform.localPosition.y - ResponseImage2.transform.GetComponent<RectTransform>().rect.height - 10);
+        }
+        
+    }
+
+    void ShowAndCloseConversation() {
+        if (isShowingConversation == false)
+        {
+            ConversationContainer.gameObject.SetActive(false);
+        }
+        else {
+            ConversationContainer.gameObject.SetActive(true);
+            updateTextLayout();
+        }
     }
 }
